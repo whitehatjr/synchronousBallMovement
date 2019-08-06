aaa8cf4872c7e3592951522ba5e8a4756cac62d4
whitehatjr <52084703+whitehatjr@users.noreply.github.com>
2019-08-07 01:55:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+var hypnoticBall, database;
+var position;
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+  hypnoticBall = createSprite(250,250,10,10);
+  hypnoticBall.shapeColor = "red";
+
+
+  var hypnoticBallPosition = database.ref('ball/position');
+  hypnoticBallPosition.on("value", readPosition, showError);
+}
+
+function draw(){
+  background("white");
+  
+    if(keyDown(LEFT_ARROW)){
+      writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+      writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+      writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+      writePosition(0,+1);
+    }
+    drawSprites();
+  
+}
+
+function writePosition(x,y){
+  database.ref('ball/position').set({
+    'x': position.x + x ,
+    'y': position.y + y
+  })
+}
+
+function readPosition(data){
+  position = data.val();
+  console.log(position.x);
+  hypnoticBall.x = position.x;
+  hypnoticBall.y = position.y;
+}
+
+function showError(){
+  console.log("Error in writing to the database");
+}
